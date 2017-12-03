5a63f5aaf64d9b5a47d2095d55e00bfc7cb450db
Chrysostomos Marasinou <cmarasinou@Chrysostomoss-MacBook-Pro.local>
2017-12-02 17:01:51 -0800

    New tab Live Demo added. Form of live demo constructed. Submit returns a probability on fixed hour and week day.  Pretrained model in data/models (gitignore).

--- web_development/main.py
+++ web_development/main.py
@@ -5,8 +5,11 @@ from flask_misaka import Misaka
 import os
 import pandas as pd
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
+
+
 
 
 class NameForm(FlaskForm):
@@ -52,6 +55,65 @@ def chart():
 @app.route('/calendar')
 def calendar():
     return render_template("calendar.html")
+
+# Demo form
+
+import numpy as np
+import xgboost as xgb
+import pickle
+
+# Modalities in the order they appear in the one-hot encoding
+modalities = np.array(['CR', 'CT', 'DX', 'MG', 'MR', 'NM', 'OT', 'PR', 'PT', 'RF', 'RG', 'SR', 'US', 'XA'])
+modalities_choices = [(modal, modal) for modal in modalities] # For the label appearing full names of the modality can be found
+# Orgcodes in the order they appear in the one-hot encoding
+orgcodes = np.array(['ASM', 'AWW', 'CCHS', 'CKHC', 'JSMO', 'MBIP', 'MP', 'MP1', 'MP1P', 'MP2P', 'MP3',
+ 'RCPN', 'RICP', 'SHC', 'SMH', 'SMO', 'SMWG', 'WWH'])
+orgcodes_choices = [(org, org) for org in orgcodes]
+feature_names = ['Age', 'Weekday', 'OrgCode_ASM', 'OrgCode_AWW', 'OrgCode_CCHS', 'OrgCode_CKHC', 'OrgCode_JSMO', 'OrgCode_MBIP', 'OrgCode_MP', 'OrgCode_MP1', 'OrgCode_MP1P', 'OrgCode_MP2P', 'OrgCode_MP3', 'OrgCode_RCPN', 'OrgCode_RICP', 'OrgCode_SHC', 'OrgCode_SMH', 'OrgCode_SMO', 'OrgCode_SMWG', 'OrgCode_WWH', 'Modality_CR', 'Modality_CT', 'Modality_DX', 'Modality_MG', 'Modality_MR', 'Modality_NM', 'Modality_OT', 'Modality_PR', 'Modality_PT', 'Modality_RF', 'Modality_RG', 'Modality_SR', 'Modality_US', 'Modality_XA', 'hour', 'Gender']
+
+class DemoForm(FlaskForm):
+    name = StringField('Name of the patient', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    gender = SelectField('Gender', choices = [('1', 'Male'), ('0', 'Female')])
+    modality = SelectField('Exam Modality', choices = modalities_choices)
+    orgcode = SelectField('Organization Code', choices = orgcodes_choices)
+    #date = DateField('What is the date?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+def preprocess_user_data(age, gender, modality, orgcode, day, hour):
+    # Process the one-hot encoded data
+    orgcode_array = (orgcodes == orgcode)
+    modality_array = (modalities == modality)
+    # Process continuous data
+    age_array = np.array([age])
+    day_array = np.array([day])
+    hour_array = np.array([hour])
+    gender_array = np.array([gender])
+
+    X_array = np.concatenate([age_array, day_array, orgcode_array, modality_array, hour_array, gender_array])
+    X_array = np.array([X_array]).astype(float)
+    return X_array
+
+# Live demo route
+@app.route('/livedemo', methods=['GET', 'POST'])
+def livedemo():
+    form = DemoForm()
+    prob = None
+    if form.validate_on_submit():
+        age = form.age.data
+        gender = int(form.gender.data)
+        modality = form.modality.data
+        orgcode = form.orgcode.data
+        X_test = preprocess_user_data(age, gender, modality, orgcode, 1, 17)
+        # Load ML model
+        model =pickle.load(open("./data/models/XGBoostMidtermModel.dat", "rb"))
+        # Predict
+        xgdmat_test = xgb.DMatrix(data = X_test, feature_names = feature_names)
+        Y_predict = model.predict(data = xgdmat_test)
+        prob = Y_predict
+        #name = form.name.data
+        #form.name.data = ''
+    return render_template('livedemo.html', form = form, prob = prob)
 
 #@app.route('/dropdown')
 #def chart():
