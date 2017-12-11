77c61bad3586a63e165cdedca5f31b36598e7b2f
Chrysostomos Marasinou <cmarasinou@g.ucla.edu>
2017-12-11 13:42:59 -0800

    Remove unwanted routes

--- web_development/main.py
+++ web_development/main.py
@@ -26,16 +26,8 @@ info=info[['Modality','Age','OrgCode','Anatomy','date','Labels']]
 features = ['Modality','Age','OrgCode','Anatomy']
 feature_tup = [(feature, feature) for feature in features]
 
-
-
-#class ChartForm(FlaskForm):
-#    orgcode = SelectField('Organization Code', choices = orgcodes_choices)
-#    submit = SubmitField('Submit')
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
+# Load hospital_map
+hospital_map = pickle.load(open("./data/hospital_map.dat", "rb"))
 
 content = ""
 with open("12.6 Algorithm Refinement.md", "r") as f:
@@ -47,48 +39,12 @@ app.config['SECRET_KEY'] = 'iridescent'
 bootstrap = Bootstrap(app)
 Misaka(app) # To use markdown in the template
 
-"""
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
-"""
-
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name=name)
-
 
 @app.route('/home')
 def home():
     contents = content
     contents = Markup(markdown.markdown(contents))
     return render_template('home.html',content = contents)
-
-@app.route('/BarCharts')
-def bar_chart():
-    labels = ["January","February","March","April","May","June","July","August"]
-    values = [10,9,8,7,6,4,7,8]
-    return render_template('barChart.html', values=values, labels=labels)
-
-@app.route('/LineCharts')
-def line_chart():
-    labels = ["January","February","March","April","May","June","July","August"]
-    values = [10,9,8,7,6,4,7,8]
-    return render_template('lineChart.html', values=values, labels=labels)
-
-@app.route('/PieCharts')
-def pie_chart():
-    labels = ["January","February","March","April","May","June","July","August"]
-    values = [10,9,8,7,6,4,7,8]
-    colors = [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC"  ]
-    return render_template('pieChart.html', set=zip(values, labels, colors))
 
 
 @app.route('/for_orgcode_charts_json')
@@ -165,44 +121,9 @@ def preproc_stacked(feature='Modality'):
 
     return labels, data, stack
 
-class FeatForm(FlaskForm):
-    feature = SelectField('Features', choices = feature_tup)
-    submit = SubmitField('Submit')
-
-@app.route('/StackedCharts', methods=['GET', 'POST'])
-def stacked_chart():
-    form = FeatForm()
-
-    if form.validate_on_submit():
-        feature = form.feature.data
-        #feature = request.args.get("selector", 'Modality')
-        labels, data, stack = preproc_stacked(info, feature)
-        return render_template('StackedChart.html', labels=labels, data=data, stack=stack, form=form)
-    labels, data, stack = preproc_stacked(info, features[0])
-    return render_template('StackedChart.html', labels=labels, data=data, stack=stack, form=form)
-
-@app.route('/calendar')
-def calendar():
-    return render_template("calendar.html")
 
 
-# Live demo route
-#@app.route('/livedemo', methods=['GET', 'POST'])
-#def livedemo():
-#    # form instance
-#    form = lvdm.DemoForm()
-    # Initialize variables
-#    prediction_html_table = None
-#    predicted = False
-#    if form.validate_on_submit():
-        # Extract form data
-#        age, gender, modality, orgcode = lvdm.process_demo_form(form)
-        # Send data to get prediction table
-#        prediction_html_table = lvdm.predict_week(age, gender, modality, orgcode)
-#        predicted = True
 
-# Load hospital_map
-hospital_map = pickle.load(open("./data/hospital_map.dat", "rb"))
 
 @app.route('/modalities_json')
 def modalities_json():
@@ -250,23 +171,8 @@ def calendar_json():
     }
     return jsonify(result = table)
 
-@app.route('/_patient_json')
-def patient_json():
-    exam_id = request.args.get('exam_id')
-    # Look in database for the exam_id and pull information of the patient
-    patient_info = {
-        'name': "John",
-        'telephone': 1112223333,
-        'email': 'john@gmail.com',
-        'gender': 'Male',
-        'age': 83
-    }
-    return jsonify(result = patient_info)
 
-
-
-# Retrieve data from database
-# Let's set up just a list for now
+# Retrieve data from hospital map
 orgcodes = ['Choose']+list(hospital_map.keys())
 orgcodes_choices = [(org, org) for org in orgcodes]
 class FiltersForm(FlaskForm):
@@ -274,42 +180,12 @@ class FiltersForm(FlaskForm):
     modality = SelectField('Exam Modality', choices = [('Choose', 'Choose')])
     departmentcode = SelectField('Department Code', choices = [('Choose', 'Choose')])
 
-class PatientForm(FlaskForm):
-    age = IntegerField('Age', validators=[DataRequired()])
-    gender = SelectField('Gender', choices = [('1', 'Male'), ('0', 'Female')])
-    submit = SubmitField('Submit')
 
 @app.route('/')
 def dashboard():
     form = FiltersForm()
-    patient_form = PatientForm()
     return render_template('dashboard.html', form =form)
 
-
-@app.route('/_render_calendar')
-def calendar_data():
-    orgcode = 'ASM' #orgcode = request.args.get('orgcode')
-    modality = 'CR' #modality = request.args.get('modality')
-    timeslots, days = find_timeslots(orgcode, modality)
-    return jsonify(timeslots = timeslots, days = days)
-
-def find_timeslots(orgcode, modality):
-    # Retrieve data from database
-    # Let's set up just a list for now
-    timeslots = ["08:00", "10.00", "15:00"]
-    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
-    return timeslots, days
-
-def render_calendar(orgcode, modality):
-
-    return None
-
-#@app.route('/dropdown')
-#def chart():
-#    labels = ["January","February","March","April","May","June","July","August"]
-#    values = [10,9,8,7,6,4,7,8]
-#    return render_template('chart.html', values=values, labels=labels)
 
 
 if __name__ == "__main__":
